ead83cfc38df6d26b9651359d901900b4a509199
Rosangel593 <rosangel593@gmail.com>
2024-02-07 10:04:40 -0300

    Create Home

---
+++ src/views/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Footer = () => {
+    return(
+                <footer className="py-3 bg-dark">
+                    <p className="text-center text-light">Copyright © newworldprogramers.com </p>
+                </footer>
+    )
+}
+export default Footer;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,27 @@
 import { useNavigate } from "react-router-dom";
-
+import Navbar from "./Navbar.js";
+import Footer from "./Footer.js";
 
 export const Home = () => {
     const navigate = useNavigate()
     return(
+        <>
+        <Navbar />
         <div className="container">
+            
             <button onClick={() => {
                 navigate("/login")
             }}>Go to login</button>
+
+
         </div>
+
+
+
+        <Footer />
+        </>
     )
 }
+
+
+export default Home;
