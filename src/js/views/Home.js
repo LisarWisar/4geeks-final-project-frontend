696e37b8e3670e7fad7af0f867931460a6a66389
Rosangel593 <rosangel593@gmail.com>
2024-02-12 18:16:52 -0300

    Created about us

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import TopNavbar from "../components/TopNavbar.js";
 import BottomNavbar from "../components/BottomNavbar.js";
 import HeroSection from "../components/HeroSection.js";
+import About from "../components/About.js";
 import AboutTeam from "../components/AboutTeam.js";
 import MissionAndVission from "../components/MissionAndVission.js";
 import ContactUs from "../components/ContactUs.js";
@@ -12,52 +13,11 @@ export const Home = () => {
     return(
         <>
         <TopNavbar />
-
         <HeroSection/>
-        The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
+        <About/>
         <MissionAndVission/>
-        The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
         <ContactUs/>
-        The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
         <AboutTeam/>
-        The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
-    The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
-    The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
-    The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
-    The spelling mistakes in the text had been highlighted in green.
-    The text is finished, but the pictures will have to be pasted in later.
-    You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-    The students are reading "Lord of the Flies" as one of their set texts this year.
-    At this point in the speech, the minister departed from his prepared text.
-    
         <BottomNavbar />
         </>
 
