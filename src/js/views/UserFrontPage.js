3c773317364486aa28bdc7b0d6599a43457d0343
LisarWisar <vsoto.zoro@gmail.com>
2024-02-22 19:37:30 -0300

    Added pet cards and appointments card to user front page

--- src/js/views/UserFrontPage.js
+++ src/js/views/UserFrontPage.js
@@ -39,6 +39,69 @@ export const UserFrontPage = () => {
         GetUserData()
       }, []);
 
+    function PetCards () {
+        let pets = []
+        for (let i = 0; i<Math.min(petsDataLength,4); i++){
+            pets.push(petsData[i])
+        }
+
+        return(
+            pets.map(pet => (
+                <div>
+                    <div className="container-fluid">
+                        <div className="py-3 d-flex">
+                            <div className="appointmentCardDate">
+                                    <div className="ratio ratio-1x1">
+                                        <img src={pet?.image}></img>
+                                    </div>
+                            </div>
+                            <div className="appointmentCardInfo py-2">
+                                <div className="row">
+                                    <div className="col-4 px-3 py-2">Name: {pet?.name}</div>
+                                    <div className="col-4 px-3 py-2">Species: {pet?.species}</div>
+                                    <div className="col-4 px-3 py-2">Age: {pet?.age} years old</div>
+                                    <div className="col-12 px-3 py-2 d-flex justify-content-end">
+                                        <button className="mx-5" onClick={() => {navigate("/user/pets")}}>See more..</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))
+        )
+    }  
+
+    function AppointmentCards () {
+        let appointments = []
+        for (let i = 0; i<Math.min(appointmentsDataLength,4); i++){
+            appointments.push(appointmentsData[i])
+        }
+
+        return(
+            appointments.map(appointment => (
+                <div>
+                    <div className="container-fluid">
+                        <div className="row py-3">
+                            <div className="col-2 appointmentCardDate d-flex flex-column align-items-center py-1">
+                                <p className="d-flex justify-content-center p-0 m-0">Thu 25</p>
+                                <p className="d-flex justify-content-center p-0 m-0">{appointment?.time}</p>
+                            </div>
+                            <div className="col-10 appointmentCardInfo py-2">
+                                <div className="row">
+                                    <div className="col-6 px-3 py-2">Veterinarian: {appointment?.veterinarian}</div>
+                                    <div className="col-6 px-3 py-2">Type of visit: {appointment?.type_of_visit}</div>
+                                    <div className="col-6 px-3 py-2">Species: {appointment?.species} </div>
+                                    <div className="col-6 px-3 py-2">Breed: {appointment?.breed}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>))
+        )
+}
+
+
     return(
         <div>
             <Navbar/>
@@ -50,7 +113,18 @@ export const UserFrontPage = () => {
                         </div>
                         <div className="col-8 vetPreviewCard my-4">
                             <p>Your pets</p>
+                            <PetCards />
+                            <div className="d-flex justify-content-center">
+                                <button>See all Pets</button>
+                            </div>
                         </div>
+                        <div className="col-8 vetPreviewCard my-4">
+                        <p>Next appointments</p>
+                        <AppointmentCards />
+                        <div className="d-flex justify-content-center">
+                            <button>See full calendar</button>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
