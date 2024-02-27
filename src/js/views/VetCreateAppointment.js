988430a48136f59411a9848f89a254e3b19c4ac5
LisarWisar <vsoto.zoro@gmail.com>
2024-02-27 17:26:26 -0300

    Vet create appointment fully functional

--- src/js/views/VetCreateAppointment.js
+++ src/js/views/VetCreateAppointment.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect} from 'react';
 export const VetCreateAppointment = () => {
 
     const navigate = useNavigate()
+    const token = localStorage.getItem('jwt-token')
     const todayDate = new Date()
     const todayDateConstant = {
         "day": String(todayDate.getDate()).padStart(2, '0'),
@@ -14,8 +15,8 @@ export const VetCreateAppointment = () => {
         "year": todayDate.getFullYear()
     }
     const [appointmentData, setAppointmentData] = useState({
-        "day": todayDateConstant.day, "month": todayDateConstant.month, "year": todayDateConstant.year,
-        "vet_id": "", "pet_id":"", "time":"" , "comments": "", "type_of_visit": ""})
+        "day": todayDateConstant.day, "month": todayDateConstant.month_ISO, "year": todayDateConstant.year,
+        "vetID": "", "petID":"", "time":"" , "comments": "", "typeOfVisit": ""})
     const [veterinariansInfo, setVeterinariansInfo] = useState()
     const [veterinariansInfoLength, setVeterinariansInfoLength] = useState()
     const [petsInfo, setPetsInfo] = useState()
@@ -37,35 +38,34 @@ export const VetCreateAppointment = () => {
         })
     }
 
+    async function SendAppointmentInfo () {
+        await fetch('http://localhost:5007/vet/calendar/create-appointment', {method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+                /*'Authorization': 'Bearer ' + token //authorization token*/
+            },
+          body: JSON.stringify({
+            appointmentData
+          })})
+          .then (response => {
+            return response.json()
+        })
+        .then(data => {
+            if (data.status == "ok"){
+                navigate("/vet/calendar")
+            }
+            else{
+                console.log(data.status)
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
     useEffect(() => {
         GetAppointmentInfo()
       }, []);
-
-    function VeterinarianSelect () {
-        let veterinariansSelect = []
-        for (let i = 0; i<veterinariansInfoLength; i++){
-            veterinariansSelect.push(veterinariansInfo[i])
-        }
-
-        return(
-            veterinariansSelect.map(vet =>(
-                <option value={vet?.vet_id}>{vet?.name}</option>
-            ))
-        )
-    }
-
-    function PetsSelect () {
-        let petsSelect = []
-        for (let i = 0; i<petsInfoLength; i++){
-            petsSelect.push(petsInfo[i])
-        }
-
-        return(
-            petsSelect.map(pet =>(
-                <option value={pet?.pet_id}>{pet?.name}</option>
-            ))
-        )
-    }
 
     const handleChange = e => {
         const { name, value } = e.target;
@@ -75,15 +75,27 @@ export const VetCreateAppointment = () => {
         }));
     };
 
+    let veterinariansSelect = []
+    for (let i = 0; i<veterinariansInfoLength; i++){
+        veterinariansSelect.push(veterinariansInfo[i])
+    }
+
+    let petsSelect = []
+    for (let i = 0; i<petsInfoLength; i++){
+        petsSelect.push(petsInfo[i])
+    }
+
     let daySelect = []
     const numDays = (y, m) => new Date(y, m, 0).getDate();
     for (let i = 1; i <numDays(appointmentData?.year, appointmentData.month)+1; i++){
-        daySelect.push(i)}
+        let dayString = `${i}`
+        daySelect.push(dayString.padStart(2, "0"))}
 
     const monthsListed= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let monthsSelect = []
     for (let i = 0; i <12; i++){
-        monthsSelect.push({"month_number": "0"+(i+1), "month_name": monthsListed[i]})
+        let monthString = `${i+1}`
+        monthsSelect.push({"month_number": monthString.padStart(2, "0"), "month_name": monthsListed[i]})
     }
 
     let yearSelect = []
@@ -100,6 +112,16 @@ export const VetCreateAppointment = () => {
         }
     }
 
+    function SubmitVerification (){
+        if (appointmentData.day !=false && appointmentData.month != false && appointmentData.year != false && appointmentData.petID != false /*Comments are not required*/
+            && appointmentData.time != false && appointmentData.typeOfVisit != false && appointmentData.vetID != false){
+            return(true)
+        }
+        else{
+            return(false)
+        }
+    }
+
     /*Return view function */
     return(
         <div>
@@ -110,16 +132,20 @@ export const VetCreateAppointment = () => {
                         <form>
                             <div className="mb-3">
                                 <label for="vetCreateAppointmentVet" class="form-label">Veterinarian</label>
-                                <select class="form-select" id="vetCreateAppointmentVet" name="vet_id" required onChange={handleChange}>
+                                <select class="form-select" id="vetCreateAppointmentVet" name="vetID" required onChange={handleChange}>
                                     <option selected disabled value="">Choose veterinarian</option>
-                                    <VeterinarianSelect/>
+                                    {veterinariansSelect.map(vet =>(
+                                        <option value={vet?.vet_id}>{vet?.name}</option>
+                                    ))}
                                 </select>
                             </div>
                             <div className="mb-3">
                                 <label for="vetCreateAppointmentPet" class="form-label">Veterinarian</label>
-                                <select class="form-select" id="vetCreateAppointmentPet" name="pet_id" required onChange={handleChange}>
+                                <select class="form-select" id="vetCreateAppointmentPet" name="petID" required onChange={handleChange}>
                                     <option selected disabled value="">Choose pet</option>
-                                    <PetsSelect/>
+                                    {petsSelect.map(pet =>(
+                                        <option value={pet?.pet_id}>{pet?.name}</option>
+                                    ))}
                                 </select>
                             </div>
                             <div className="row">
@@ -133,8 +159,8 @@ export const VetCreateAppointment = () => {
                                 <div className="mb-3 col-4">
                                     <label for="vetCreateAppointmentMonth" className="form-label">Month</label>
                                     <select class="form-select" id="vetCreateAppointmentMonth" name="month" required onChange={handleChange}>
-                                        <option selected disabled value={todayDateConstant.month_ISO}>{monthsListed[todayDateConstant.month]}</option>
-                                        {monthsSelect.map(month =>(<option value={month.month_number}>{month.month_name}</option>))}
+                                        <option selected disabled value={todayDateConstant?.month_ISO}>{monthsListed[todayDateConstant?.month]}</option>
+                                        {monthsSelect.map(month =>(<option value={month?.month_number}>{month?.month_name}</option>))}
                                     </select>
                                 </div>
                                 <div className="mb-3 col-4">
@@ -160,10 +186,16 @@ export const VetCreateAppointment = () => {
                                 <label for="vetCreateAppointmentTypeOfVisit" className="form-label">Type of visit</label>
                                 <input name="typeOfVisit" type="text" className="form-control" id="vetCreateAppointmentTypeOfVisit" required onChange={handleChange}/>
                             </div>
-                            <button type="submit" className="btn btn-primary" onClick={() => {
-                                console.log("data: ",appointmentData)
-                            }}>Submit</button>
                         </form>
+                        <button type="submit" className="btn btn-primary" onClick={() => {
+                            if (SubmitVerification() == true){
+                                console.log("data: ",appointmentData)
+                                SendAppointmentInfo()
+                            }
+                            else{
+                                console.log("Missing information")
+                            }
+                        }}>Submit</button>
                     </div>
                 </div>
             </div>
