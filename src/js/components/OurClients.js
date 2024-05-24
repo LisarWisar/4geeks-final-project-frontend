91f4759f8c0184fe2643e855a9a9811181536236
LisarWisar <vsoto.zoro@gmail.com>
2024-05-24 03:07:42 -0400

    Corrected code formatting on OurClients.js

--- src/js/components/OurClients.js
+++ src/js/components/OurClients.js
@@ -4,26 +4,25 @@ import "../../styles/OurClients.css";
 export const OurClients = () => {
   return (
     <>
-<div className="title-h1">
-    <h1 className='h1-title' id="section3">Our Clients</h1>
-    </div>
-
-        <div className="container">
-          <div className="top-images">
-            <img src={require("../images/client-1.jpg")} alt="client-1" width={"350px"} height={"250px"} />
-            <img src={require("../images/client-2.jpg")} alt="client- 2" width={"350px"} height={"250px"} />
-            <img src={require("../images/client-3.jpg")} alt="client- 3" width={"350px"} height={"250px"} />
-          </div>
+      <div className="title-h1">
+        <h1 className='h1-title' id="section3">Our Clients</h1>
+      </div>
+      <div className="container">
+        <div className="top-images">
+          <img src={require("../images/client-1.jpg")} alt="client-1" width={"350px"} height={"250px"} />
+          <img src={require("../images/client-2.jpg")} alt="client- 2" width={"350px"} height={"250px"} />
+          <img src={require("../images/client-3.jpg")} alt="client- 3" width={"350px"} height={"250px"} />
         </div>
-        <div className="container">
-          <div className="bottom-images">
-            <img src={require("../images/client-4.jpg")} alt="client- 4" width={"350px"} height={"250px"} />
-            <img src={require("../images/client-5.jpg")} alt="client- 5" width={"350px"} height={"250px"} />
-            <img src={require("../images/client-6.jpg")} alt="clien-6" width={"350px"} height={"250px"} />
-          </div>
+      </div>
+      <div className="container">
+        <div className="bottom-images">
+          <img src={require("../images/client-4.jpg")} alt="client- 4" width={"350px"} height={"250px"} />
+          <img src={require("../images/client-5.jpg")} alt="client- 5" width={"350px"} height={"250px"} />
+          <img src={require("../images/client-6.jpg")} alt="clien-6" width={"350px"} height={"250px"} />
         </div>
-      </>
-      )
+      </div>
+    </>
+  )
 }
 
 export default OurClients;
