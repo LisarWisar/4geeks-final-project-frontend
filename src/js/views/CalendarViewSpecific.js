d0f82afdb8e6c07dc8f13569aeba74d6b223bea6
LisarWisar <vsoto.zoro@gmail.com>
2024-05-24 02:42:29 -0400

    Fixed formatting mistakes on CalendarViewSpecific.js

--- src/js/views/CalendarViewSpecific.js
+++ src/js/views/CalendarViewSpecific.js
@@ -5,54 +5,48 @@ import about from "../images/about.jpg";
 export const CalendarViewSpecific = () => {
   return (
     <>
-    <div>CalendarViewSpecific</div>
-    
-    <div className="petBody">
-    <div className="d-flex justify-content-center">
-    
-                    </div>
-                    <div className="col-8 pet-card my-4 py-5">
-                        <div className="row">
-                            <div className="col-9">
-                                <div className="date-info">
+        <div>CalendarViewSpecific</div>
+            <div className="petBody">
+                <div className="col-8 pet-card my-4 py-5">
+                    <div className="row">
+                        <div className="col-9">
+                            <div className="date-info">
                                 <div className="row">
-                                <div className="col-6"><h1>Saturday 27</h1></div>
+                                    <div className="col-6"><h1>Saturday 27</h1></div>
                                     <div className="col-6">15:00 </div>
                                     <div className="col-6">Species: </div>
                                     <div className="col-6">Date of birth: </div>
                                     <div className="col-6">Age:  years old</div>
-                                    </div>
                                 </div>
-                            </div>
                             </div>
                         </div>
-                    <div className="col-8 pet-card my-4 py-5">
-                        
-                            <div className="col-3">
-                                <div className="ratio ratio-1x1" id="image-pet">
-                                    <img  src= { about } alt='calendar-img'></img>
-                                </div>
-                            </div>
-                            
-                            <div className="container-pet">
-                                <div id="data-pet">
-                                <div className="col-9">
+                    </div>
+                </div>
+                <div className="col-8 pet-card my-4 py-5">
+                    <div className="col-3">
+                        <div className="ratio ratio-1x1" id="image-pet">
+                            <img  src= { about } alt='calendar-img'></img>
+                        </div>
+                    </div>
+                    <div className="container-pet">
+                        <div id="data-pet">
+                            <div className="col-9">
                                 <div className="row">
                                     <div className="col-6" >Pet Name: </div>
                                     <div className="col-6">Species: </div>
                                     <div className="col-6">Breed </div>
                                     <div className="col-6">Owner Name</div>
-                                    </div>
-                                    <div className='text-area'> 
+                                </div>
+                                <div className='text-area'> 
                                     <p>Aditional Information</p>
                                     <textarea  placeholder='Type here..'/>
-                                    </div>
-                                    </div>
-                                    </div>
-                            </div>
+                                </div>
                             </div>
                         </div>
-                    </>
+                    </div>
+                </div>
+        </div>
+    </>
   )
 }
 export default CalendarViewSpecific;
